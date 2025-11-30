9c4b6994e59a5b803056cfd94849e98ffd1efafd
braintumordetection10 <braintumordetection10@gmail.com>
2025-11-30 07:59:00 +0500

    Update app.py

--- app.py
+++ app.py
@@ -216,5 +216,6 @@ def contact():
 # RUN
 # ---------------------------------------------------
 if __name__ == "__main__":
-    app.run()
+    import os
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
 
